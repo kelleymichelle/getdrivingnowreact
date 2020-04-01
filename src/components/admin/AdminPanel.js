a5bbe7587739e03235f0e62c71e0e69b60f15de5
Kelley Chaplain <kmchaplain@gmail.com>
2020-04-01 16:55:10 -0500

    admin login can post to backend. Nothing happenening there yet but routing properly at least

--- src/components/admin/AdminPanel.js
+++ src/components/admin/AdminPanel.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import AdminLogin from './AdminLogin'
+import axios from 'axios'
 
 export default class AdminPanel extends React.Component {
+  state = {
+    loggedIn: false
+  }
   
   handleLogin = data => {
     console.log(data)
+    const {email, password} = data
+    let user = {
+      email: email,
+      password: password
+    }
+
+    axios.post('http://localhost:3001/admin_login', { user }, {withCredentials: true})
+    .then(response => console.log(response))
   }
 
   render() {
+    
     return (
-      <AdminLogin handleLogin={this.handleLogin}/>
+        <AdminLogin handleLogin={this.handleLogin}/>
     )
+    
   }
 }
