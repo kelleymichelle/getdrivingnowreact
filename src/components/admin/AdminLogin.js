29095d6a95fbcc2de6279d670cc34420b2806158
Kelley Chaplain <kmchaplain@gmail.com>
2020-04-01 15:24:59 -0500

    admin login component working all except for submit

--- src/components/admin/AdminLogin.js
+++ src/components/admin/AdminLogin.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 export default class AdminLogin extends React.Component {
 
@@ -8,9 +10,33 @@ export default class AdminLogin extends React.Component {
     email: '',
     password: ''
   }
+
+  handleOnChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+  }
+
   render() {
     return (
+      <div style={{width: '300px', margin: '50px'}}>
       <h1>Admin Login</h1>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control id="email" type="text" name="email" value={this.state.email} onChange={this.handleOnChange}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Control id="password" type="password" name="password" value={this.state.password} onChange={this.handleOnChange}/>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
+      </div>
     )
   }
 }
